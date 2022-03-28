e482b84619359eba24e9ec514fd00528ee4262c1
Ayush Yadav <ayushyadav468@gmail.com>
2022-03-28 12:24:18 +0530

    email-sent page added

--- sections/loginPage/LoginPage.tsx
+++ sections/loginPage/LoginPage.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import { useRef } from 'react';
+import { useRouter } from 'next/router';
 
 import LightMobileLogo from 'assets/logo/small_white_dashed_logo.svg';
 import LoginPageSvg from 'assets/sections/login/login_page.svg';
+import Emoji from 'components/emoji/Emoji';
 
 const LoginPage = () => {
+	const router = useRouter();
 	const emailRef = useRef<HTMLInputElement>(null);
 
 	// handle login submit of for magic email
@@ -12,7 +15,9 @@ const LoginPage = () => {
 		event.preventDefault();
 
 		if (emailRef.current) {
-			// emailRef.current.value;
+			//* call api
+			emailRef.current.value = '';
+			router.push('/email-sent');
 		}
 	};
 
@@ -39,24 +44,25 @@ const LoginPage = () => {
 				<div className='w-max max-w-sm flex flex-col justify-start items-start gap-6'>
 					<h1 className='text-2xl text-secondary font-semibold'>
 						Welcome to Drozee!
+						<Emoji symbol='&#128075;' label='waving hand emoji' />
 					</h1>
 					<p className='text-primary'>
 						Please login to your account. Enter your email below and we will
 						send you an email. No need to register. We will handle the rest.
 					</p>
-					<form className='w-full'>
+					<form className='w-full' onSubmit={handleSubmit}>
 						<label className='w-full text-primary'>
 							Email
 							<input
 								type='email'
-								className='w-full mt-2 border-[1px] border-light rounded py-2 px-4 text-base outline-none focus:border-primary'
+								className='w-full mt-3 border-[1px] border-light rounded py-2 px-4 text-base outline-none focus:border-primary'
 								placeholder='Enter your email'
-								required
+								ref={emailRef}
 							/>
 						</label>
 						<button
 							type='submit'
-							className='w-full mt-4 py-3 px-4 bg-primary text-center text-white rounded'
+							className='w-full mt-6 py-3 px-4 bg-primary text-center text-white rounded'
 						>
 							Send an magic email
 						</button>
