a745a616c85b1db800ad5efcc7de6a12a53ef857
Ayush Yadav <ayushyadav468@gmail.com>
2023-04-13 12:27:23 +0530

    project directory structure updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,10 @@ const withOpacity = (colorVariable) => {
 
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./layout/**/*.{js,ts,jsx,tsx}',
-		'./sections/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}'
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/layout/**/*.{js,ts,jsx,tsx}',
+		'./src/sections/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
 		extend: {
